6c8e0743d298c71c00958baaf1c512aaa2a08c16
Jagathpriyan <bluegriffen1856@gmail.com>
2025-02-12 14:01:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Jagathpriyan</h1>
+      </div>
+      <div>
+        <h2>PH:9972033536</h2>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
